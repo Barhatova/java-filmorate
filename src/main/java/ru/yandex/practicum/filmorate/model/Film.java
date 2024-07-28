fc9491e0a58790648ac464f9e5fae866cb267e12
Ирина <barhatov4.i@yandex.ru>
2024-07-28 19:22:14 +0300

    Реализовать ТЗ 11 спринта(устранить замечания ревью..)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,9 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Data
