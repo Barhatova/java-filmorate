7b8fd1fe45d2998ee0a284fe7ba6eb1f5cda7ec4
Ирина <barhatov4.i@yandex.ru>
2024-08-26 08:29:08 +0300

    Реализовать ТЗ 12 спринта.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
